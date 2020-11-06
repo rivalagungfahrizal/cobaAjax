b0acb5f8c2bc930fca3acf3c0c3d171f5dc55c69
rival <rivalagungfahrizal@mahasiswa.unikom.ac.id>
2020-11-06 16:33:17 +0700

    tambah jquery + hide tombol cari

--- latihan/basicphp/databasemysql/index.php
+++ latihan/basicphp/databasemysql/index.php
@@ -90,7 +90,7 @@ if (isset($_POST["reset"])) {
       <?php endforeach; ?>
     </table>
   </div>
-
+  <script src="js/jquery-3.5.1.min.js"></script>
   <script src="js/script.js"></script>
 </body>
 
--- latihan/basicphp/databasemysql/js/script.js
+++ latihan/basicphp/databasemysql/js/script.js
@@ -1,23 +1,38 @@
-//ambil elemen2 yang dibutuhkan
-var keyword = document.getElementById('keyword');
-var tombolCari = document.getElementById('tombol_cari');
-var tombolReset = document.getElementById('tombol_reset');
-var container = document.getElementById('container');
 
-//tambah event ketika keyword ditulis
-keyword.addEventListener('keyup', function () {
+//ajax
 
-  //buat object ajax
-  var xhr = new XMLHttpRequest();
+// //ambil elemen2 yang dibutuhkan
+// var keyword = document.getElementById('keyword');
+// var tombolCari = document.getElementById('tombol_cari');
+// var tombolReset = document.getElementById('tombol_reset');
+// var container = document.getElementById('container');
 
-  //cek kesiapan ajax
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      container.innerHTML = xhr.responseText;
-    }
-  }
+// //tambah event ketika keyword ditulis
+// keyword.addEventListener('keyup', function () {
 
-  //eksekusi ajax
-  xhr.open('GET', 'ajax/mahasiswa.php?keyword=' + keyword.value, true);
-  xhr.send();
+//   //buat object ajax
+//   var xhr = new XMLHttpRequest();
+
+//   //cek kesiapan ajax
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       container.innerHTML = xhr.responseText;
+//     }
+//   }
+
+//   //eksekusi ajax
+//   xhr.open('GET', 'ajax/mahasiswa.php?keyword=' + keyword.value, true);
+//   xhr.send();
+// });
+
+$(document).ready(function () {
+
+  //hilangkan tombol cari
+  $('#tombol_cari').hide();
+
+  //event ketika keyword ditulis
+  $('#keyword').on('keyup', function () {
+    $('#container').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
+  });
+
 });
